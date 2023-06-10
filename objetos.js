cd6759658fb0226c22df90b6855659097facb3b9
omarbustamantegarcia2004 <135771650+omarbustamantegarcia2004@users.noreply.github.com>
2023-06-09 19:11:25 -0600

    objetos.js

---
+++ objetos.js
@@ -0,0 +1,17 @@
+¿Qué es JSON?
+JSON, cuyas siglas significan en verdad JavaScript object notation que, en español se traducen como, notación de objetos de JavaScript, es un formato de intercambio de datos que resulta muy fácil de leer y escribir para los programadores y sencillo de interpretar y crear para las máquinas.
+JSON es un formato de texto totalmente independiente del lenguaje de programación, pero utiliza términos que son comúnmente conocidos por los programadores, entre ellas:
+C
+C++
+C#
+Java
+JavaScrip
+¿Qué es JSON?
+Perl
+Python, entre otros.
+Según la descripción de Stack Overflow, JSON:
+“Define seis tipos de valores: nulo, números, cadenas, booleanos, matrices y objetos”.
+Sin embargo, no es necesariamente parte de JavaScript. De hecho, es un estándar basado en texto plano que sirve para intercambiar datos y se usa cuando se requiere que un sistema requiera mostrar o enviar información para que otros sistemas los lean e interpreten.
+En resumen, JSON no es un lenguaje de programación sino un archivo que contiene datos estructurados, que se utiliza para transferir información entre sistemas.
+¿Cómo funciona?
+Una de las características más significativas de JSON, al ser un formato independiente de los lenguajes de programación, es que los servicios que comparten información por este método no necesitan hablar el mismo idioma. Es decir que el emisor y el receptor pueden ser totalmente distintos, por ejemplo, Java y Python. Esto es así porque cada uno tiene su propia librería de codific
