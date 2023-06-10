76cf22cce2a7e1397b1bcff727d4df9b3fe98a1f
omarbustamantegarcia2004 <135771650+omarbustamantegarcia2004@users.noreply.github.com>
2023-06-09 19:09:07 -0600

    Arreglos.js

--- Arreglos.js
+++ Arreglos.js
@@ -1,4 +1,4 @@
-const Barcelona = ["pedri","jordi","messi","marc"]
-console.log(Barcelona);
-Barcelona[0] = "Lewandowski";
-console.log(Barcelona);
+const verduleria = ["manzana","pera","uva","durazno"]
+console.log(verduleria);
+Barcelona[0] = "mango con chamoy";
+console.log(verduleria);
